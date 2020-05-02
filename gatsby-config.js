010d538094ee8e8726cfc0cdc1d5a643c06c7ac2
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-05-02 17:54:17 +0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'GatsbyTSTW',
+    titleTemplate: '%s | GatsbyTSTW',
+    description: 'Starter boilerplate for Gatsby with Typescript and TailwindCSS',
+    author: 'Fidz.Dev',
+    url: 'https://fidz.dev', // No trailing slash allowed!
+    image: '/app-banner.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: 'sohafidz',
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyTSTW`,
+        short_name: `GatsbyTSTW`,
+        start_url: `/`,
+        background_color: `#F7FAFC`,
+        theme_color: `#2B6CB0`,
+        display: `standalone`,
+        icon: `static/app-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
